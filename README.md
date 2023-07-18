53fed5943702028d8c6a6540b865a6627d12f88e
BaileyKau <88602557+BaileyKau@users.noreply.github.com>
2023-07-18 10:20:19 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a Google Extension WIP; When completed, it should utilize Madgrades and RateMyProfessor APIs to show students the average rating of the professor and the average GPA. More features to come!
+
+
+TODO: 
+
+FIGURE OUT THE ISSUE WHERE THE CONTENT ONLY UPDATES WHEN YOU ACCIDENTALLY BACK OUT OF THE PAGE
+manually update whenever click? will look into this
+
+PROFESSOR THING, FIND OUT HOW TO GET PROFESSOR
+
+CONNECT TO API
---
+++ contentScript.js
@@ -0,0 +1,27 @@
+    const courseSelected = () => {
+        //Getting the course title
+        var courseTitle = document.getElementsByClassName("course-title")[0];
+        if (courseTitle) { courseTitle = courseTitle.innerHTML; }
+
+        //Madgrades API 
+        fetch("https://api.madgrades.com/v1/instructors?query=cs252", {
+            method: 'GET',
+            headers: {
+                'Authorization' : "Token token=6cd3473136464c3d854e2f7c5372807c",
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+
+        //Getting the professors
+
+        //Rate My Professor
+    }
+    
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        // console.log("Message received! Yippee!");
+        const { message } = obj;
+        if (message === 'NEW'){
+            courseSelected();
+        }
+    });
